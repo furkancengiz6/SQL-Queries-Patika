790d04f8a44ee0ce179016dc8c672a5b566f5f5c
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-24 14:34:03 +0300

    Update SqlQuery7.sql

--- SQL-WORK-7/SqlQuery7.sql
+++ SQL-WORK-7/SqlQuery7.sql
@@ -1,13 +1,12 @@
 --1.
 SELECT rating,COUNT(*) FROM film
-GROUP BY rating
+GROUP BY rating;
 
---2.film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
-SELECT replacement_cost ,
+--2.
 COUNT (*) 
 FROM film
 GROUP BY replacement_cost 
-HAVING COUNT(*) > 50
+HAVING COUNT(*) > 50;
 
 --3.
 SELECT store_id, 
