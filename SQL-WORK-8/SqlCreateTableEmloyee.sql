d06ee167d5dc60a73efa7ed63fe7edf289976ae4
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-27 14:58:51 +0300

    Update SqlCreateTableEmloyee.sql

--- SQL-WORK-8/SqlCreateTableEmloyee.sql
+++ SQL-WORK-8/SqlCreateTableEmloyee.sql
@@ -1,6 +1,6 @@
 CREATE TABLE employee (
     id INTEGER PRIMARY KEY,
-    name VARCHAR(50),
-    birthday DATE,
+    name VARCHAR(50) NOT NULL,
+    birthday DATE NOT NULL,
     email VARCHAR(100)
 );
