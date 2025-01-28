7c89232e3fc73ecd17b89cc528f78a6e7ad4ac8f
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-01-28 17:27:12 +0300

    Add files via upload

---
+++ SQL-WORK-12/SqlQueryes12.sql
@@ -0,0 +1,25 @@
+--1.film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+--SELECT COUNT(*) AS film_sayisi
+--FROM film 
+
+--WHERE length >(SELECT AVG(length) FROM film);
+
+--2.Film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) AS film_sayisi
+FROM film 
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+--3.Film tablosunda en düşük rental_rate ve en düşük replacement_cost değerlerine sahip filmleri sıralayınız.
+
+SELECT *
+FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
+  AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+--4.Payment tablosunda en fazla sayıda alışveriş yapan müşterileri (customer) sıralayınız.
+SELECT customer_id, COUNT(*) AS toplam_odeme
+FROM payment
+GROUP BY customer_id
+ORDER BY toplam_odeme DESC;
+
+
